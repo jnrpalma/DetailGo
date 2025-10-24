57644caa2f072a790dd61614e66728b3d1d92ded
jnrpalma <jribeiropalma@gmail.com>
2025-10-24 14:14:59 -0300

    cria user adm

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,4 +2,7 @@ export type RootStackParamList = {
   Login: undefined;
   Register: undefined;
   Dashboard: undefined;
+  Appointment: undefined;
+  AdminDashboard: undefined; // novo
+  Admin: undefined;          // tela "Gerenciar"
 };
