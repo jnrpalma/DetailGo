4f3355f05f8d4f7f298043eb1871193045418f22
jnrpalma <nicolas.jnr10@outlook.com>
2026-02-13 01:50:40 -0300

    feat(appontment): ajusta screen

--- src/features/appointments/domain/appointment.pricing.ts
+++ src/features/appointments/domain/appointment.pricing.ts
@@ -1,3 +1,4 @@
+// src/features/appointments/domain/appointment.pricing.ts
 import type { VehicleType, CarCategory } from './appointment.types';
 
 export const CAR_BASE_PRICE_BY_CATEGORY: Record<CarCategory, number> = {
